9e9482ca333ea3ce9266ac6563e7de4919d86007
Ken Cruz <ken@kencruz.ca>
2021-08-21 12:22:10 -0400

    converted init to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+-- init.lua
+
+-- load plugins
+vim.cmd('source $HOME/.config/nvim/vim-plug/plugins.vim')
+-- load settings
+vim.cmd('source $HOME/.config/nvim/settings/general.vim')
+-- load mappings
+vim.cmd('source $HOME/.config/nvim/settings/mappings.vim')
+
+-- just for vim-lsc-dart
+-- let g:lsc_auto_map = v:true
